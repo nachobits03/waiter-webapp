41818133333267b5d6a84638139b13a1607eee66
nachowolf <nathrijacobs@gmail.com>
2022-01-24 17:01:16 +0200

    added modal to allow users to select which days of the week they'll be working

--- public/javascript/shift.js
+++ public/javascript/shift.js
@@ -1,19 +1,31 @@
-window.addEventListener('DOMContentLoaded', function () {
-    let button = document.querySelectorAll('.button');
-    let shift = document.querySelectorAll('#shift');
 
-    for (let i = 0; i <= button.length; i++) {
 
-        function shiftCheck () {
-            if (shift[i].checked === false) {
-                shift[i].checked = true;
-                button[i].classList.remove('oldchecked');
-                button[i].classList.add('checked');
-            } else if (shift[i].checked === true) {
-                button[i].classList.remove('checked');
-            }
-        }
+// window.addEventListener('DOMContentLoaded', function () {
+//     let button = document.querySelectorAll('.button');
+//     let shift = document.querySelectorAll('#shift');
 
-        button[i].addEventListener('click', shiftCheck);
-    }
-});
+//     for (let i = 0; i <= button.length; i++) {
+
+//         function shiftCheck () {
+//             if (shift[i].checked === false) {
+//                 shift[i].checked = true;
+//                 button[i].classList.remove('oldchecked');
+//                 button[i].classList.add('checked');
+//             } else if (shift[i].checked === true) {
+//                 button[i].classList.remove('checked');
+//             }
+//         }
+
+//         button[i].addEventListener('click', shiftCheck);
+//     }
+// });
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems, {});
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(elems, {});
+  });
